c00f2f6430cba0f082e55146d2c9f8ccb8dbca5c
AlexV <a.visan@student.fontys.nl>
2020-10-06 23:43:11 +0200

    Added Login form

--- Employee Management Alpha 1.0/LoginForm.cs
+++ Employee Management Alpha 1.0/LoginForm.cs
@@ -14,6 +14,7 @@ namespace Employee_Management_Alpha_1._0
 {
     public partial class LoginForm : Form
     {
+        MySqlConnection conn = new MySqlConnection("server=studmysql01.fhict.local;database=dbi360075;uid=dbi360075;password=monke;");
         public LoginForm()
         {
             InitializeComponent();
@@ -21,10 +22,32 @@ namespace Employee_Management_Alpha_1._0
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            this.Hide();
-            Form1 main = new Form1();
-            main.Closed += (s, args) => this.Close();
-            main.Show();
+
+            string sql = "SELECT * FROM login";
+            bool correct = false;
+            MySqlCommand cmd = new MySqlCommand(sql, this.conn);
+            conn.Open();
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                if(txtUsername.Text == dr[0].ToString() && txtPassword.Text == dr[1].ToString())
+                {
+                    correct = true;
+                }
+            }
+
+            if (correct)
+            {
+                this.Hide();
+                Form1 main = new Form1();
+                main.Closed += (s, args) => this.Close();
+                main.Show();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect login credentials! Check username/password!");
+            }
         }
     }
 }
