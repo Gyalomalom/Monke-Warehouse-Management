d9673adf5637fe2b2fc8cf8d15999d5094a081d1
Aleksandar_Nasev <aleknasev@yahoo.com>
2020-10-06 21:54:33 +0200

    Minor changes to department UI

--- Employee Management Alpha 1.0/DepartmentInfo.Designer.cs
+++ Employee Management Alpha 1.0/DepartmentInfo.Designer.cs
@@ -99,7 +99,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(161, 48);
             this.button1.TabIndex = 5;
-            this.button1.Text = "Disable";
+            this.button1.Text = "Mark Inactive";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
