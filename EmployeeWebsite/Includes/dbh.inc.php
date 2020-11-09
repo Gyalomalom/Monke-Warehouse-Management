9eba0f807890f6bdc4fa89e62b432d24ce6c68a4
David Horvath <david.horvath@student.fontys.nl>
2020-11-09 16:03:07 +0100

    FOLDERS

---
+++ EmployeeWebsite/Includes/dbh.inc.php
@@ -0,0 +1,9 @@
+<?php
+$dbServername = "studmysql01.fhict.local";
+$dbUsername = "dbi360075";
+$dbPassword = "monke";
+$dbName = "dbi360075";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+?>
