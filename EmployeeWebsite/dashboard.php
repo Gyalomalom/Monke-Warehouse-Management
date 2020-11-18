d6c5ac1c436231ec452fa6b0c6f5605a3da4d10b
David Horvath <david.horvath@student.fontys.nl>
2020-11-18 15:17:47 +0100

    home page html added

---
+++ EmployeeWebsite/dashboard.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <title>Employee dashboard</title>
+<link rel="stylesheet" type="text/css" href="Styles/Style.css">
+</head>
+<body>
+<?php include 'navbar.html'; ?>
+
+<div class = "dashwrapper">
+<div class="container">
+<br>
+            Phone: +4206969
+            <br>
+</div>
+<div class="container">
+<br>
+            Phone: +4206969
+            <br>
+</div>
+</div>
+
+</body>
+
+<footer><?php include 'footer.php'; ?></footer>
+</html>
